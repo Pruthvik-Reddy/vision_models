e94a21c2659293aeed3924fad0d913247df3a62f
Pruthvik-Reddy <pruthvikreddy.21@gmail.com>
2024-04-02 01:41:24 -0400

    inference

--- UniChart-main/inference.py
+++ UniChart-main/inference.py
@@ -32,35 +32,46 @@ for filename in os.listdir(path4):
 for image_path in png_files:
 
 
+    input_prompts=[
+        "<chartqa> Explain the Graph <s_answer>",
+        "<chartqa> Explain the Chart <s_answer>",
+        "<opencqa> Explain the Graph <s_answer>",
+        "<opencqa> Explain the Chart <s_answer>",
+        "<summarize_chart> <s_answer>",
+        "<summarize_chart> Explain the Graph <s_answer>",
+        "<summarize_chart> Explain the Chart <s_answer>",
+        
+    ]
+    
     #image_path = "./content/chart_example_1.png"
-    input_prompt = "<summarize_chart> Give Summary <s_answer>"
+    #input_prompt = "<summarize_chart> Give Summary <s_answer>"
+    for input_prompt in input_prompts:
+        model = VisionEncoderDecoderModel.from_pretrained(model_name)
+        processor = DonutProcessor.from_pretrained(model_name)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        model.to(device)
 
-    model = VisionEncoderDecoderModel.from_pretrained(model_name)
-    processor = DonutProcessor.from_pretrained(model_name)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+        image = Image.open(image_path).convert("RGB")
+        decoder_input_ids = processor.tokenizer(input_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+        pixel_values = processor(image, return_tensors="pt").pixel_values
 
-    image = Image.open(image_path).convert("RGB")
-    decoder_input_ids = processor.tokenizer(input_prompt, add_special_tokens=False, return_tensors="pt").input_ids
-    pixel_values = processor(image, return_tensors="pt").pixel_values
+        outputs = model.generate(
+            pixel_values.to(device),
+            decoder_input_ids=decoder_input_ids.to(device),
+            max_length=model.decoder.config.max_position_embeddings,
+            early_stopping=True,
+            pad_token_id=processor.tokenizer.pad_token_id,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            use_cache=True,
+            num_beams=4,
+            bad_words_ids=[[processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True,
+        )
+        sequence = processor.batch_decode(outputs.sequences)[0]
+        print(image_path)
+        print(sequence)
 
-    outputs = model.generate(
-        pixel_values.to(device),
-        decoder_input_ids=decoder_input_ids.to(device),
-        max_length=model.decoder.config.max_position_embeddings,
-        early_stopping=True,
-        pad_token_id=processor.tokenizer.pad_token_id,
-        eos_token_id=processor.tokenizer.eos_token_id,
-        use_cache=True,
-        num_beams=4,
-        bad_words_ids=[[processor.tokenizer.unk_token_id]],
-        return_dict_in_generate=True,
-    )
-    sequence = processor.batch_decode(outputs.sequences)[0]
-    print(image_path)
-    print(sequence)
-
-    #sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
-    #print(sequence)
-    #sequence = sequence.split("<s_answer>")[1].strip()
-    #print(sequence)
+        #sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+        #print(sequence)
+        #sequence = sequence.split("<s_answer>")[1].strip()
+        #print(sequence)
